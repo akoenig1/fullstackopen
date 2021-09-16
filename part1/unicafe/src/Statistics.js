41a12c64272cd54c56b8a52f16876b9a99e3e317
Alex Koenig <alex.koenig4@gmail.com>
2021-09-15 23:20:33 -0400

    Exercise 1.9

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -8,16 +8,18 @@ const Statistics = (props) => {
   const all = props.all
   const score = props.score
   const positive = props.positive
+
+  const ratings = <>
+    <Rating field="Good" count={good} />
+    <Rating field="Neutral" count={neutral} />
+    <Rating field="Bad" count={bad} />
+    <Rating field="All" count={all} />
+    <Rating field="Average" count={all > 0 ? (score / all) : 0} />
+    <Rating field="Positive" count={all > 0 ? ((positive / all) * 100) : 0} unit="%" />
+  </>
   
   return (
-    <>
-      <Rating field="Good" count={good} />
-      <Rating field="Neutral" count={neutral} />
-      <Rating field="Bad" count={bad} />
-      <Rating field="All" count={all} />
-      <Rating field="Average" count={all > 0 ? (score / all) : 0} />
-      <Rating field="Positive" count={all > 0 ? ((positive / all) * 100) : 0} unit="%" />
-    </>
+    all > 0 ? ratings : "No Feedback Given"
   )
 }
 
