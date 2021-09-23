e0d4c9dade3037bf09cceb19affb64692d001cd1
Alex Koenig <alex.koenig4@gmail.com>
2021-09-23 19:59:25 -0400

    Exercise 1.12

---
+++ part1/anecdotes/src/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({handleClick}) => {
+  const button = <button onClick={handleClick} >next anectode</button>
+  
+  return(
+    button
+  )
+}
+
+export default Button
